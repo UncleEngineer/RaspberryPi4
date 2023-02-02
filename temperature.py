5eb432c1aebefbcf27aa33ed22ed123283d694c1
Uncle Engineer <loong.wissawakorn@gmail.com>
2023-02-02 21:41:20 +0700

    EP.11

--- temperature.py
+++ temperature.py
@@ -2,12 +2,37 @@ import random
 import csv
 import Adafruit_DHT
 import time
+import RPi.GPIO as GPIO
+
+##########LCDLIB###########
+import lcdlib
+lcd = lcdlib.lcd()
+############RELAY############
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+relay = 17 # GPIO17
+GPIO.setup(relay, GPIO.OUT)
+
 
 def writetocsv(data,filename='data'):
     # data = [25.66,50.66]
     with open('{}.csv'.format(filename) ,'a',newline='',encoding='utf-8') as file:
         fw = csv.writer(file) # fw = file writer
         fw.writerow(data)
+        
+def TurnOn(sleep=0):
+    GPIO.output(relay, False)
+    if sleep > 0:
+        time.sleep(sleep)
+        
+def TurnOff(sleep=0):
+    GPIO.output(relay, True)
+    if sleep > 0:
+        time.sleep(sleep)
+
+# TurnOff at Start
+TurnOff()
+
 
 class Sensor:
     """ Sensor Class for IoT """
@@ -15,7 +40,9 @@ class Sensor:
         self.name = name
         self.type = 'DHT22'
         self.DHT_SENSOR = Adafruit_DHT.DHT22
-        self.DHT_PIN = 2
+        self.DHT_PIN = 27
+        self.current_temp = 0
+        self.current_humid = 0
 
     def get_temp_humid(self):
         h, t = Adafruit_DHT.read_retry(self.DHT_SENSOR, self.DHT_PIN)
@@ -23,7 +50,15 @@ class Sensor:
 
     def show_result(self, writecsv=False):
         temp, humid = self.get_temp_humid()
-        print('Temperature: {:.2f}°C\nHumidity: {:.2f}%'.format(temp,humid))
+        self.current_temp = temp
+        self.current_humid = humid
+        #
+        text_temp = 'Temp: {:.2f}-C'.format(temp)
+        text_humid = 'Humid: {:.2f}%'.format(humid)
+        #print(text_temp)
+        lcd.lcd_display_string(text_temp,1)
+        lcd.lcd_display_string(text_humid,2)
+        #print('Temperature: {:.2f}°C\nHumidity: {:.2f}%'.format(temp,humid))
 
         if writecsv == True:
             data = [temp,humid]
@@ -32,9 +67,16 @@ class Sensor:
 
 temp1 = Sensor('DHT22-101')
 
-for i in range(10):
-    temp1.show_result(True)
-    print('------')
+while True:
+    try:
+        temp1.show_result(True)
+        if temp1.current_temp >= 30:
+            TurnOn()
+        else:
+            TurnOff()
+    except:
+        pass
+    #print('------')
     time.sleep(2)
 
 
